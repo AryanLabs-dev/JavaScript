e6f193f00919c57a56b0de46f99be4756ec7b5b4
Aryan Singh <coxaryan.singh@gmail.com>
2026-07-30 21:37:50 +0530

    First.js Completed Successfully 🎉✅

---
+++ style.js
@@ -0,0 +1,12 @@
+const profile = {
+    Name: "Shradhakhapra",
+    posts: "195",
+    Followers: "596k",
+    Following: "4",
+    title: "Shraddha Khapra",
+    Position: "Entrepreneur",
+    isfollow: false,
+}
+ 
+console.log(profile)
+console.log(typeof profile)
