7403f8b5c105153a00b0d69d4cf9e9eef96a9c37
Aryan Singh <coxaryan.singh@gmail.com>
2026-08-10 18:08:55 +0530

    More On Javascript
    it takes a lot's of time but now it will be completed SuccessFully. ✅

---
+++ Practice.js
@@ -0,0 +1,12 @@
+let guess= 45;
+Numguess = prompt("Enter Your Number Here!");
+
+while (Numguess != guess) {
+    Numguess=prompt("You Are Wrong! ❌. Try Again!")
+}
+console.log("Congratulations! 🎊, You Entered Correct Number")
+
+
+let MyName = prompt("Enter Your Name");
+let username = "@" + MyName;
+console.log(username.length); 
---
+++ index.js
@@ -0,0 +1,53 @@
+// for (let Aryan = 0; Aryan < 100; Aryan++) {
+//     console.log("Hello Aryan")
+// } // This Loop Called For loop;
+
+// // Here is Infite Loop Example
+// for (let Aryan = 0; Aryan >= 0; Aryan++) {
+//     console.log("Hello, Founder. Mr Aryan")
+// }
+// // Sorry but Don't Run this code in your System because it will create an infinite loop and your system will hang.😅
+// let a = 1;
+// while (a >= 0) {
+//     console.log("Hello, Mr.Aryan Sir 😊 your Ai is here!")
+// }
+// // It also an infinite loop so don't run this code in your system because it will create an infinite loop and your system will hang.😅
+
+// // Guy's here is an practice project for You to print 1 to 100, using any type og loop, and thaks for watching my ropository. 😊 
+// // here is solution of this project using for loop,
+// for (let Aryan = 0; Aryan < 100; Aryan++) {
+//     console.log(Aryan + "Welcome, Mr Aryan!")
+// }
+
+
+// for (let Aryan = 0; Aryan < 101; Aryan++) {
+//     console.log(Aryan + " Welcome, Mr Aryan")
+// }
+// for (let Aryan = 0; Aryan < 100; Aryan++) {
+//     console.log(Aryan + 1)
+// }
+// for (let Aryan = 0; Aryan < 100; Aryan++) {
+//  if (Aryan%2 === 0) {
+//     console.log("Number Value " + Aryan )
+//  }
+// }
+// // this loop for only even number form 0 to 100.
+//  for (let Aryan = 0; Aryan < 100; Aryan++) {
+//     if (Aryan%2 != 0) {
+//         console.log("Number Value " + Aryan)
+//     }
+//  }
+// // this loop for only odd number form 0 to 100.
+// // Q2.
+// let Numbers = 39;
+// let Guess = prompt("Enter Your Number!")
+// while (Guess != Numbers) {
+//     Guess = prompt("Your Guess Is Wrong! ❌ Try Again!")
+// }
+// console.log("Your Guess Is Correct! ✅ Congratulations!")
+// let number = "JavaScript";
+
+// for (const Aryan of number) {
+//     console.log(Aryan)
+// }
+
