e6dac6fbc49c2d579288b066b0729f6d38a87861
Aryan Singh <coxaryan.singh@gmail.com>
2026-08-21 22:51:05 +0530

    Events in Javascript❤️
    Events in javascript, Completed Successfully✅

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-// Practice Q1. 🟡
-// let div = document.createElement("h1")
-// div.innerHTML = "<i>Hello, Aryan sir!</i>"
-// document.querySelector("body").prepend(div)
+let mode = document.querySelector("#btn")
+let mymode = "lighter"
+let body = document.querySelector("body")
 
-//Practice Q2. 🟢
-let div = document.createElement("button")
-div.innerHTML = "Click me"
-document.querySelector("body").prepend(div)
-div.style.color = "white"
-div.style.backgroundColor = "red"
+mode.addEventListener("click", () => {
+    if (mymode === "lighter") {
+        mymode = "darkar"
+        document.body.classList.add("darkar")
+    }
+    else{
+        mymode = "lighter"
+        document.body.classList.remove("darkar")
+    }
+    console.log(mymode)
+})
 
-//Practice Q3. 🔴
-let parahs = document.querySelector("p")
-parahs.classList.add("content")
+ let dev = document.querySelector("btn");
+dev.addEventListener("click", () => {
+    console.log("You are trying to change mode")
+
+})
+
+// let mode = document.querySelector("#btn")
+
+// let mymode = "lighter"
+
+// let body = document.querySelector("body")
+
+// mode.addEventListener("click", () => {
+
+//     if (mymode === "lighter") {
+
+//         mymode = "darkar"
+
+//         document.body.classList.add("darkar")
+//     }
+//     else {
+//         mymode = "lighter";
+
+//         document.body.classList.remove("darkar")
+//     }
+//     // console.log("You're trying to changing the mode")
+//     console.log(mymode)
+// })
